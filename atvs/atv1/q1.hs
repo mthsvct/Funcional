5e22088fca858da65caa987cc66c302d0fb60ec3
Matheus Victor <matheusvictor@ufpi.edu.br>
2022-12-04 19:32:09 -0300

    questao1 resolvida!!

---
+++ atvs/atv1/q1.hs
@@ -0,0 +1,22 @@
+ehTriangulo(a, b, c) | (a + b > c) && (b + c > a) && (a + c > b) = True
+                | otherwise = False
+
+-- ehEscaleno(a, b, c) | (a /= b) && (b /= c) && (a /= c) = True
+
+calcula(a, b, c) = do
+        let p = (a + b + c)
+        let s = p / 2
+        let area = sqrt(s * (s - a) * (s - b) * (s - c))
+        putStr "Area: "
+        putStrLn(show(area))
+
+area(a,b,c) | ehTriangulo(a, b, c) = calcula(a, b, c)
+        | otherwise = putStrLn "Nao eh triangulo"
+
+main = do
+        {- 
+            area(2, 3, 4) = Eh triangulo e retorna um valor
+            area(1, 2, 3) = Nao eh triangulo e nao retorna nada
+        -}
+        putStrLn "Hello!"
+
