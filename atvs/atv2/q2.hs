9723c960ee21cb0d32fc5e6b7d27fae1588e9535
Matheus Victor <matheusvictor@ufpi.edu.br>
2022-12-08 23:12:00 -0300

    resolução da questão 2 da atv 2

---
+++ atvs/atv2/q2.hs
@@ -0,0 +1,13 @@
+media(a, b, c) = (a + b + c) / 3
+
+ehIgual(x, m) | x == m = 1
+            | otherwise = 0
+
+calcula(a, b, c) = do
+            let m = media(a, b, c)
+            let total = ehIgual(a, m) + ehIgual(b, m) + ehIgual(c, m)
+            putStrLn (show total)
+
+main :: IO ()
+main = do
+        putStrLn "Hello!"
