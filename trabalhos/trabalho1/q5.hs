991611cd5f23477694921423c87490b3a5154c3b
Matheus Victor <matheusvictor@ufpi.edu.br>
2022-12-21 18:32:06 -0300

    Mudanças

--- trabalhos/trabalho1/q5.hs
+++ trabalhos/trabalho1/q5.hs
@@ -1,11 +1,4 @@
 -- QuickSort em Haskell
--- Função que recebe uma lista e retorna a lista ordenada
-
--- Função que recebe uma lista e retorna a lista ordenada
-{- quickSort [] = []
-quickSort (x:xs) = quickSort [y | y <- xs, y <= x] ++ [x] ++ quickSort [y | y <- xs, y > x] -}
-
--- Função simplificada que recebe uma lista e retorna a lista ordenada
 quickSort(lista)
         | null lista == True = []
         | otherwise = quickSort [y | y <- tail lista, y <= head lista] 
