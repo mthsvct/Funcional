2e237a27d5200ae34617d6c62bb6134e6834643f
Matheus Victor <matheusvictor@ufpi.edu.br>
2022-12-23 18:42:43 -0300

    forma de calcular e percorrer uma lista em haskell

--- funcoes/listas/listas_manuais.hs
+++ funcoes/listas/listas_manuais.hs
@@ -6,6 +6,12 @@ produto(x, i)
         | i < length x = (x !! i) * produto(x, i+1)
         | otherwise = 1
 
+-- c:r referece a c ser a cabeça da lista e r ser o resto da lista.
+somaMult3([], soma) = soma
+somaMult3(c:r, soma)
+        | mod c 3 == 0 = somaMult3(r, soma + c)
+        | otherwise = somaMult3(r, soma)
+
 geraLista(i, limite)
         | i < limite = i : geraLista(i+1, limite)
         | otherwise = []
