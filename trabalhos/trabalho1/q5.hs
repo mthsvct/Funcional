f8ecdcf3256d311b69d0fdcbb3dfa71e78d3c984
Matheus Victor <matheusvictor@ufpi.edu.br>
2022-12-27 17:22:42 -0300

    letraB da q5 do trabalho1

--- trabalhos/trabalho1/q5.hs
+++ trabalhos/trabalho1/q5.hs
@@ -1,3 +1,5 @@
+module Q5_trab1 where
+
 -- QuickSort em Haskell
 quickSort(lista)
         | null lista == True = []
@@ -5,8 +7,29 @@ quickSort(lista)
                                 ++ [head lista] 
                                 ++ quickSort [y | y <- tail lista, y > head lista]
 
+{-
+        B - uma função que devolva uma lista contendo a 
+                soma entre 
+                        os quadrados dos elementos das duas listas
+                                que forem maiores do que a soma entre o cubo dos 
+                                dois primeiros elementos da lista.
+-}
 
+potencia (base, exp) 
+        | exp > 1 = base * potencia(base, exp-1)
+        | otherwise = base
 
+cubo(a:b:lista) = potencia(a, 3) + potencia(b, 3)
 
-main = do
-        putStrLn "Hello World"
+quadrado([], c) = []
+quadrado(a:lista, c)
+        | a > c = potencia(a, 2) : quadrado(lista, c)
+        | otherwise = quadrado(lista, c)
+
+somaQuadrados([], []) = 0
+somaQuadrados(a:lista1, []) = a
+somaQuadrados([], b:lista2) = b
+somaQuadrados(a:lista1, b:lista2) = (a + b) + somaQuadrados(lista1, lista2)
+
+soma(a, b) = somaQuadrados( quadrado(a, cubo(a)), quadrado(b, cubo(b)) )
+
