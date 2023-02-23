c93c7dfcd5f50dcc23f137716a2b570a8cd3b19d
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-02-23 11:07:13 -0300

    letra A da q3 do trab2

--- src/trabalhos/trabalho2/q3.hs
+++ src/trabalhos/trabalho2/q3.hs
@@ -4,19 +4,24 @@ module Q3_trab2 where
 -}
 
 
--- A) devolva duas listas, a primeira contendo os números das posições pares maiores do que 50 e a segunda os elementos ímpares menores que 200.
-adicionaFinal([], y) = [y]
-adicionaFinal(c:r, y) = c : adicionaFinal(r, y)
+-- A) devolva duas listas, a primeira contendo os números das posições pares maiores do que 50 
+-- e a segunda os elementos ímpares menores que 200.
 
-maiores50(c, lista, i)
-    | mod i 2 == 0 && c > 50 = adicionaFinal(lista, c)
-    | otherwise = lista
 
-impares200(c, lista)
-    | mod c 2 /= 0 && c < 200 = adicionaFinal(lista, c)
-    | otherwise = lista
+monta50(c, i, rL1)
+    | mod i 2 == 0 && c > 50 = c:rL1
+    | otherwise = rL1
 
-busca(c1:l1, c2:l2, i1, i2, m50, im200)
-    
-    
-principalA(l1, l2) = busca(l1, l2, 0, 0, [], [])
+monta200(c, i, rL1)
+    | i `mod` 2 == 1 && c < 200 = c:rL1
+    | otherwise = rL1
+
+montaListas([], i, [], j, l50, l200) = (reverse(l50), reverse(l200))
+montaListas([], i, c2:l2, j, l50, l200) = montaListas([], i, l2, j+1, monta50(c2, j, l50), monta200(c2, j, l200))
+montaListas(c:l1, i, [], j, l50, l200) = montaListas(l1, i+1, [], j, monta50(c, i, l50), monta200(c, i, l200))
+montaListas(c:l1, i, c2:l2, j, l50, l200)
+    | c < c2 = montaListas(l1, i+1, c2:l2, j, monta50(c, i, l50), monta200(c, i, l200))
+    | c > c2 = montaListas(c:l1, i, l2, j+1, monta50(c2, j, l50), monta200(c2, j, l200))
+    | otherwise = montaListas(l1, i+1, l2, j+1, monta50(c, i, l50), monta200(c, i, l200))
+
+principalA(l1, l2) = montaListas(l1, 0, l2, 0, [], [])
