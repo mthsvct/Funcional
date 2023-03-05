0871e4a91d8a79ff09f058bb3eb414cf9becc44a
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-03-05 00:58:27 -0300

    2

---
+++ src/atvs/atv_extra/q2.hs
@@ -0,0 +1,13 @@
+import Data.Char
+
+insereOrdenado(e, []) = [e]
+insereOrdenado(e, c:r)
+    | e == c = c:r
+    | e < c = e:c:r
+    | otherwise = insereOrdenado(e, r)
+
+
+caracteresMaiusculos([], aux) = aux
+caracteresMaiusculos(c:r, aux)
+    | isUpper( c ) = caracteresMaiusculos(r, insereOrdenado(c, aux))
+    | otherwise = caracteresMaiusculos(r, aux)
