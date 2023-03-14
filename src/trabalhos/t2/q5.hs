e515eed79d0299cbe01b1022ec1eb5eedaf54540
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-03-14 14:14:18 -0300

    Cadastro e apresentação de notas cadastradas

--- src/trabalhos/t2/q5.hs
+++ src/trabalhos/t2/q5.hs
@@ -28,6 +28,9 @@ cursos_ex = [ (1, "Ciência da Computação", 10),(2, "Engenharia de Software", 
 alunos_ex :: [Aluno]
 alunos_ex = [(1, "Matheus Victor", 1, 2), (2, "João Pedro", 1, 2), (3, "Pedro Henrique", 1, 2), (4, "Gabriel Henrique", 1, 2), (5, "Arthur Miguel", 1, 2), (6, "Bernardo", 1, 2), (7, "Heitor", 1, 2), (8, "Davi Lucas", 1, 2), (9, "Lorenzo", 1, 2), (10, "Enzo Gabriel", 1, 2), (11, "Murilo", 2, 2), (12, "Luiz Felipe", 2, 2), (13, "Benjamin", 2, 2), (14, "Guilherme", 2, 2), (15, "Samuel", 2, 2), (16, "Nicolas", 2, 2), (17, "Daniel", 2, 2), (18, "Matheus", 2, 2), (19, "Henrique", 2, 2), (20, "Gustavo", 2, 2), (21, "Felipe", 3, 2), (22, "Lucas", 3, 2), (23, "Theo", 3, 2), (24, "Lorenzo", 3, 2), (25, "Benjamin", 3, 2), (26, "Davi", 3, 2), (27, "João", 3, 2), (28, "Guilherme", 3, 2), (29, "Murilo", 3, 2), (30, "Heitor", 3, 2), (31, "Arthur", 4, 2), (32, "Pedro", 4, 2), (33, "Gabriel", 4, 2), (34, "Enzo", 4, 2), (35, "Matheus", 4, 2), (36, "Lorenzo", 4, 2), (37, "Davi", 4, 2), (38, "Benjamin", 4, 2), (39, "João", 4, 2), (40, "Guilherme", 4, 2), (41, "Murilo", 5, 2), (42, "Luiz Felipe", 5, 2), (43, "Benjamin", 5, 2), (44, "Guilherme", 5, 2), (45, "Samuel", 5, 2), (46, "Nicolas", 5, 2), (47, "Daniel", 5, 2), (48, "Matheus", 5, 2), (49, "Henrique", 5, 2), (50, "Gustavo", 5, 2), (51, "Felipe", 6, 2), (52, "Lucas", 6, 2), (53, "Theo", 6, 2), (54, "Lorenzo", 6, 2), (55, "Benjamin", 6, 2), (56, "Davi", 6, 2), (57, "João", 6, 2), (58, "Guilherme", 6, 2), (59, "Murilo", 6, 2), (60, "Heitor", 6, 2), (61, "Arthur", 7, 2), (62, "Pedro", 7, 2), (63, "Gabriel", 7, 2), (64, "Enzo", 7, 2), (65, "Matheus", 7, 2), (66, "Lorenzo", 7, 2), (67, "Davi", 7, 2), (68, "Benjamin", 7, 2), (69, "João", 7, 2), (70, "Guilherme", 7, 2), (71, "Murilo", 8, 2), (72, "Luiz Felipe", 8, 2), (73, "Benjamin", 8, 2), (74, "Guilherme", 8, 2), (75, "Samuel", 8, 2), (76, "Luiza", 9, 6)]
 
+disciplinas_ex :: [Disciplina]
+disciplinas_ex = [(1, 2, "Engenharia de Software", 4), (2, 2, "Programação Orientada a Objetos", 4), (3, 2, "Banco de Dados", 4), (4, 2, "Sistemas Operacionais", 4), (5, 2, "Redes de Computadores", 4), (6, 2, "Programação Web", 4), (7, 2, "Programação para Dispositivos Móveis", 4), (8, 2, "Programação para Dispositivos Móveis", 4), (9, 2, "Programação para Dispositivos Móveis", 4), (10, 2, "Programação para Dispositivos Móveis", 4), (11, 2, "Programação para Dispositivos Móveis", 4), (12, 2, "Programação para Dispositivos Móveis", 4), (13, 2, "Programação para Dispositivos Móveis", 4), (14, 2, "Programação para Dispositivos Móveis", 4), (15, 2, "Programação para Dispositivos Móveis", 4), (16, 2, "Programação para Dispositivos Móveis", 4), (17, 2, "Programação para Dispositivos Móveis", 4), (18, 2, "Programação para Dispositivos Móveis", 4), (19, 2, "Programação para Dispositivos Móveis", 4), (20, 2, "Programação para Dispositivos Móveis", 4), (21, 2, "Programação para Dispositivos Móveis", 4), (22, 2, "Programação para Dispositivos Móveis", 4), (23, 2, "Programação para Dispositivos Móveis", 4), (24, 2, "Programação para Dispositivos Móveis", 4), (25, 2, "Programação para Dispositivos Móveis", 4), (26, 2, "Programação para Dispositivos Móveis", 4), (27, 2, "Programação para Dispositivos Móveis",7) ]
+
 
 fst3(x, _, _) = x
 fst4(x, _, _, _) = x
@@ -99,8 +102,10 @@ lerAluno = do
     return (read matricula, nome, read codigo, read periodo)
 
 
+getNomeAluno((_, x, _, _)) = x
 getCodCursoAluno((_, _, x, _)) = x
 getPeriodoAluno((_, _, _, x)) = x
+
 
 gestaoLerAluno :: [Aluno] -> [Curso] -> IO [Aluno]
 gestaoLerAluno alunos cursos = do
@@ -132,6 +137,7 @@ gestaoApresentaAlunos lista cursos = do
         apresentaAlunos lista cursos
     return lista
 
+
 qntAlunosPorCurso([], []) = putStrLn " "
 qntAlunosPorCurso(c:qnts, c2:cursos) = do
     putStrLn ("Quantidade de alunos no curso " ++ snd3(c2) ++ ": " ++ show c)
@@ -147,8 +153,9 @@ gestaoStaticsAlunos alunos cursos = do
     putStrLn ("Quantidade de alunos por período: " ++ show qntAlunosPeriodo)
     return alunos
 
--- ----------------------------- DISCIPLINAS -----------------------------
+buscaAluno(alunos, matricula) = [x | x <- alunos, fst4(x) == matricula]
 
+-- ----------------------------- DISCIPLINAS -----------------------------
 -- Função que lê uma disciplina pelo o teclado
 
 lerDisciplina :: IO Disciplina
@@ -163,11 +170,44 @@ lerDisciplina = do
     periodo <- getLine
     return (read codigo, read codigoCurso, nome, read periodo)
 
-gestaoLerDisciplina :: IO ()
-gestaoLerDisciplina = do
-    disciplina <- lerDisciplina
-    putStrLn ("Disciplina: " ++ show disciplina)
+getCodCursoDisciplina((_, x, _, _)) = x
+getNomeDisc((_, _, x, _)) = x
 
+{- gestaoLerDisciplina :: IO () -}
+gestaoLerDisciplina disciplinas cursos = do
+    disciplina <- lerDisciplina
+    let cod = getCodCursoDisciplina(disciplina)
+    let aux = buscaCurso(cursos, cod)
+
+    if (aux == [])
+        then do
+            putStrLn "\nCurso não cadastrado\nPor favor, digite um codigo valido!\n"
+            gestaoLerDisciplina disciplinas cursos
+    else do
+        putStrLn ("Disciplina: " ++ show disciplina)
+        return (disciplina:disciplinas)
+
+
+apresentaDisciplinas [] cursos = putStrLn " "
+apresentaDisciplinas ((codigo, codigoCurso, nome, periodo):t) cursos = do
+    let c = buscaCurso(cursos, codigoCurso)
+    putStrLn ("Código da disciplina:............ " ++ show codigo)
+    putStrLn ("Nome da disciplina:.............. " ++ nome)
+    putStrLn ("Período da disciplina:........... " ++ show periodo)
+    putStrLn ("Curso da disciplina:............. " ++ snd3(head(c)))
+    putStrLn " "
+    apresentaDisciplinas t cursos
+
+
+gestaoApresentaDisciplina(disciplinas, cursos) = do
+    if (disciplinas == [])
+        then putStrLn "Não há disciplinas cadastradas"
+    else do
+        putStrLn ("------------- DISCIPLINAS CADASTRADAS ---------------")
+        apresentaDisciplinas disciplinas cursos
+    return disciplinas
+
+buscaDisciplina(codigo, disciplinas) = [x | x <- disciplinas, fst4(x) == codigo]
 
 -- ----------------------------- NOTAS ----------------------------- --
 
@@ -181,14 +221,58 @@ lerNotas = do
     codigo <- getLine
     putStr "Digite a nota 1 do aluno: "
     nota1 <- getLine
-    putStr "Digite a nota 2 do aluno: "
-    nota2 <- getLine
-    return (read matricula, read codigo, read nota1, read nota2)
 
-gestaoLerNotas :: IO ()
-gestaoLerNotas = do
-    notas <- lerNotas
-    putStrLn ("Notas: " ++ show notas)
+    putStr "\nDeseja digitar a segunda nota (S/N): "
+    op <- getLine
+
+    if (op == "S" || op == "s")
+        then do
+            putStr "Digite a nota 2 do aluno: "
+            nota2 <- getLine
+            return (read matricula, read codigo, read nota1, read nota2)
+    else do
+        return (read matricula, read codigo, read nota1, -1)
+
+{- gestaoLerNotas :: IO () -}
+gestaoLerNotas notas alunos disciplinas cursos  = do
+    nota <- lerNotas
+    let aux = buscaAluno(alunos, fst4(nota))
+    let aux2 = buscaDisciplina(snd4(nota), disciplinas)
+
+    if (aux == [])
+        then do
+            putStrLn "\nAluno não cadastrado\nPor favor, digite uma matricula valida!\n"
+            gestaoLerNotas notas alunos disciplinas cursos
+    else if (aux2 == [])
+        then do
+            putStrLn "\nDisciplina não cadastrada\nPor favor, digite um codigo valido!\n"
+            gestaoLerNotas notas alunos disciplinas cursos
+    else do
+        putStrLn ("Notas: " ++ show notas)
+        return (nota:notas)
+
+apresentaNotas :: [Notas] -> [Aluno] -> [Disciplina] -> [Curso] -> IO ()
+apresentaNotas [] alunos disciplinas cursos = putStrLn " "
+apresentaNotas ((matricula, codigo, nota1, nota2):t) alunos disciplinas cursos = do
+    let a = buscaAluno(alunos, matricula)
+    let d = buscaDisciplina(codigo, disciplinas)
+    putStrLn ("Matrícula do aluno:.............. " ++ show matricula)
+    putStrLn ("Nome do aluno:................... " ++ getNomeAluno(a!!0) )
+    putStrLn ("Código da disciplina:............ " ++ show codigo)
+    putStrLn ("Nome da disciplina:.............. " ++ getNomeDisc(d!!0) )
+    putStrLn ("Nota 1:.......................... " ++ show nota1)
+    putStrLn ("Nota 2:.......................... " ++ show nota2)
+    putStrLn " "
+    apresentaNotas t alunos disciplinas cursos 
+
+gestaoApresentaNotas :: [Notas] -> [Aluno] -> [Disciplina] -> [Curso] -> IO [Notas]
+gestaoApresentaNotas notas alunos disciplinas cursos = do
+    if (notas == [])
+        then putStrLn "Não há notas cadastradas"
+    else do
+        putStrLn ("------------- NOTAS CADASTRADAS ---------------")
+        apresentaNotas notas alunos disciplinas cursos
+    return notas
 
 
 -- ----------------------------- MENU ----------------------------- --
@@ -204,7 +288,9 @@ menu = do
     putStrLn "4 - Apresentar Estatisticas Alunos"
     putStrLn "5 - Apresentar Alunos"
     putStrLn "6 - Ler Disciplina"
-    putStrLn "7 - Ler Notas"
+    putStrLn "7 - Apresentar Disciplinas"
+    putStrLn "8 - Ler Notas"
+    putStrLn "9 - Apresentar Notas"
     putStrLn "0 - Sair"
 
 -- Função que lê a opção do menu
@@ -226,22 +312,39 @@ executarAluno(3, alunos, cursos) = gestaoLerAluno alunos cursos
 executarAluno(4, alunos, cursos) = gestaoStaticsAlunos alunos cursos
 executarAluno(5, alunos, cursos) = gestaoApresentaAlunos alunos cursos
 
+executarDisciplina :: (Int, [Disciplina], [Curso]) -> IO [Disciplina]
+executarDisciplina(6, disciplinas, cursos) = gestaoLerDisciplina disciplinas cursos
+executarDisciplina(7, disciplinas, cursos) = gestaoApresentaDisciplina(disciplinas, cursos)
+
+executarNotas :: (Int, [Notas], [Aluno], [Disciplina], [Curso]) -> IO [Notas]
+executarNotas(8, notas, alunos, disciplinas, cursos) = gestaoLerNotas notas alunos disciplinas cursos
+executarNotas(9, notas, alunos, disciplinas, cursos) = gestaoApresentaNotas notas alunos disciplinas cursos
 
 -- Função que executa o menu
-executarMenu :: [Curso] -> [Aluno] -> IO ()
-executarMenu cursos alunos = do
+executarMenu :: [Curso] -> [Aluno] -> [Disciplina] -> [Notas] -> IO ()
+executarMenu cursos alunos disciplinas notas = do
     menu
     opcao <- lerOpcao
     if opcao == 1 || opcao == 2 then do
         aux <- executarCurso(opcao, cursos)
-        executarMenu aux alunos
-    else if opcao == 3 || opcao == 4 then do
+        executarMenu aux alunos disciplinas notas
+
+    else if opcao == 3 || opcao == 4 || opcao == 5 then do
         aux2 <- executarAluno(opcao, alunos, cursos)
-        executarMenu cursos aux2
+        executarMenu cursos aux2 disciplinas notas
+
+    else if opcao == 6 || opcao == 7 then do
+        aux3 <- executarDisciplina(opcao, disciplinas, cursos)
+        executarMenu cursos alunos aux3 notas
+    
+    else if opcao == 8 || opcao == 9 then do
+        aux4 <- executarNotas(opcao, notas, alunos, disciplinas, cursos)
+        executarMenu cursos alunos disciplinas aux4
+        
     else return ()
 
 -- Função principal
 
 main :: IO ()
 main = do
-    executarMenu [] []
+    executarMenu [] [] [] []
