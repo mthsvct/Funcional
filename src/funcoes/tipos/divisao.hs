e90a09123debf60fb7ff9c661608cd548e15800f
Matheus Victor <matheusvictor@ufpi.edu.br>
2023-03-21 21:07:02 -0300

    Testes

--- src/funcoes/tipos/divisao.hs
+++ src/funcoes/tipos/divisao.hs
@@ -3,7 +3,7 @@ module Main where
 divisao :: Int -> Int -> Maybe Int
 divisao x y
     |  y == 0 = Nothing
-    | otherwise = Just(div x y)
+    |  otherwise = Just(div x y)
 
 main :: IO ()
 main = do 
@@ -16,3 +16,16 @@ main = do
             putStrLn "Tente novamente"
             main
         Just z -> putStrLn("Resposta: "++show(z))
+
+
+
+
+pega :: [a] -> Int -> Int -> Maybe a
+pega [] i n = Nothing
+pega c:r i n
+    | i == n = Just(c)
+    | otherwise = pega r (i+1)
+
+
+
+
